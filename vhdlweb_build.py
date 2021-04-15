b6fa282fc6faba288d00b9765f31ca05bf1aea0d
Michael Riegert <michael@eowyn.net>
2021-04-14 20:49:30 -0400

    Increase timeout

--- vhdlweb_build.py
+++ vhdlweb_build.py
@@ -112,6 +112,6 @@ def run_netlist(wdir):
   synthesis_output = safe_run(command + ["make", "-f", wdir + "Makefile", "--directory", wdir, "--silent"] + current_app.config['MAKE_ARGS'] + ["netlist.json"], stderr = sp.STDOUT)
 
   # Run netlistsvg locally
-  netlistsvg_output = safe_run(["netlistsvg", wdir + "netlist.json", "-o", netlist_output], stderr = sp.STDOUT)
+  netlistsvg_output = safe_run(["netlistsvg", wdir + "netlist.json", "-o", netlist_output], stderr = sp.STDOUT, timeout = 60)
 
   return synthesis_output.decode('utf-8') + '\n\n' + netlistsvg_output.decode('utf-8')
